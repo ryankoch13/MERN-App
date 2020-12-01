3dbb9f5a12eb2d1c74ccbb41c0f7f77cfdedac0a
ryankoch13 <ryan.koch13@gmail.com>
2020-12-01 09:25:13 -0800

    app started, create functionality working

---
+++ server/controllers/posts.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const PostMessage = require('../models/postMessage.js')
+
+const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+const createPost = async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save()
+
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(408).json({ message: error.message })
+    }
+}
+
+module.exports = { getPosts, createPost }
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const postRoutes = require('./routes/posts')
+const { router } = require('./routes/posts')
+
+const app = express()
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL = 'mongodb+srv://admin:adminadmin123@cluster0.az5ju.mongodb.net/admin?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`)))
+.catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
